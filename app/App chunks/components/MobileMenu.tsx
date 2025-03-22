9983605e1e44474cd796821017747ddc5dea9ea3
faheem221 <faheemk793@gmail.com>
2025-03-22 19:17:25 +0530

    fixed

--- app/App chunks/components/MobileMenu.tsx
+++ app/App chunks/components/MobileMenu.tsx
@@ -77,7 +77,7 @@ const MobileMenu = ({}) => {
               delay: 0.1,
               ease: [0.79, 0.14, 0.15, 0.86],
             }}
-            className="w-full h-screen fixed top-0 right-0 bg-black"
+            className="w-full h-screen fixed top-0 right-0 z-40 bg-black"
           >
             <motion.div
               initial={{
@@ -238,7 +238,7 @@ const Icon: React.FC<IconProps> = ({ setIsOpen, isOpen, path }) => {
   return (
     <button
       onClick={() => setIsOpen(!isOpen)}
-      className="size-10 relative z-[9999] relative z-10 rounded-full"
+      className="size-10  z-[9999] relative rounded-full"
     >
       <div className="flex hover flex-col justify-center items-center transition-all gap-1">
         <motion.div
