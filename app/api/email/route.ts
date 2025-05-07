81c2700f26aaa218ef5b6e39e94e1d277132cac2
faheem <frontendmakaidigitals@gmail.com>
2025-05-07 13:56:11 +0530

    dsfadsf

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request): Promise<Response> {
       service: "gmail",
       auth: {
         user: "frontendmakaidigitals@gmail.com",
-        pass: "zcmj wxys vzkh hron", // <-- Make sure this is the correct app password, no spaces
+        pass: "cndxfaineqdveczu", // <-- Make sure this is the correct app password, no spaces
       },
       tls: {
         rejectUnauthorized: false, // <--- this line fixes the "self-signed certificate" error
