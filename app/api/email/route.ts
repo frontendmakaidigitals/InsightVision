e36552c2d6c20d3f4b22fda162a94eed16a45727
faheem <frontendmakaidigitals@gmail.com>
2025-05-07 15:38:22 +0530

    sadfasdf

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -6,11 +6,9 @@ const resend = new Resend('re_6GLFhiTf_3javH3x77xZTr6qutop8Z1LS'); // Your Resen
 export async function POST(req: Request) {
   try {
     const { firstName, lastName, phone, email, message } = await req.json();
-    console.log("Received data:", { firstName, lastName, phone, email, message });
-    // Send the email using Resend
     const emailElement = await resend.emails.send({
       from: 'onboarding@resend.dev', // your email
-      to: 'frontendmakaidigitals@gmail.com', // recipient email
+      to: 'info@insightvision.marketing', // recipient email
       subject: `New Contact from ${firstName} ${lastName}`,
       text: `
         Name: ${firstName} ${lastName}
